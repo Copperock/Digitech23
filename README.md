523da2dbcfd824eaa35c9ae4e5fcb168b421eb4b
copperock <youc21091@wainuiomatahigh.school.nz>
2023-03-07 21:07:16 +0000

    if else statment pratise

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 #Digitech23 
 This is code for my 2023 Sem A lvl1 class.
+
+To run code that is not "main.py"
+input to the shell "python3 (file name. what ever code type)"
+deleate commas and brackets unless in file name.
+
--- if-else.py
+++ if-else.py
@@ -1,1 +1,9 @@
-print("Hello world")
+name = input("What is your name?")
+age = int(input("Hello " + name + " what is your age?"))
+
+if age < 18:
+  print("Lets stay in")
+
+else:
+  print("Lets go out for some fun or somthing!")
+
