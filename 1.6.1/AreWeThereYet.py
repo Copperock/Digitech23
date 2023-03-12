ae7efc575b057bf3950ee3b731ea6a23e2951e23
Copperock <youc21091@wainuiomatahigh.school.nz>
2023-03-12 21:25:19 +0000

    1.6.1

--- 1.6.1/AreWeThereYet.py
+++ 1.6.1/AreWeThereYet.py
@@ -1,4 +1,16 @@
+AreWeThereyetstat = False
 def AreWeThereyet():
   AreWeThereyet = input("Are we there yet? (Yes,No)")
-  if AreWeThereyet == "Yes" :
+  if AreWeThereyet == "Yes" or AreWeThereyet == "yes" :
     print ("WOOOOOOOOO")
+    AreWeThereyetstat = True
+    quit()
+  elif AreWeThereyet == "no" or AreWeThereyet == "No":
+    print ("Then when?")
+    AreWeThereyetstat = False
+  else:
+    print("Thats not a bloody anwser, ya Jeremy Clarkson looking fool!")
+    AreWeThereyetstat = False
+
+while AreWeThereyetstat == False:
+  AreWeThereyet()
