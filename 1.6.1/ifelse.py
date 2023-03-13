999a733ab97828580b206ae0319797dfef5744df
Replit user <>
2023-03-13 23:24:32 +0000

    A few changes for the joy

---
+++ 1.6.1/ifelse.py
@@ -0,0 +1,22 @@
+### This is mostly a code from "AreWeThereYet.py" with a slight fun modifacation. ###
+
+AreWeThereyetstat = False
+def AreWeThereyet():
+  AreWeThereyet = input("Are we there yet? (Yes,No)")
+  if AreWeThereyet == "Yes" or AreWeThereyet == "yes" :
+    print ("WOOOOOOOOO")
+    AreWeThereyetstat = True
+    quit()
+  elif AreWeThereyet == "no" or AreWeThereyet == "No":
+    print ("Then when?")
+    AreWeThereyetstat = False
+  elif AreWeThereyet == "Sir you are drunk" or AreWeThereyet == "Are you drunk?" :
+    print ("I am sorry sir, but you are over the legal limit.  You must now accompany me to the station")
+    AreWeThereyetstat = True
+    quit()
+  else:
+    print("Thats not a bloody anwser, ya Jeremy Clarkson looking fool!")
+    AreWeThereyetstat = False
+
+while AreWeThereyetstat == False:
+  AreWeThereyet()
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-#Digitech23 
+# Digitech23 
 This is code for my 2023 Sem A lvl1 class.
 
+---
 To run code from repl you will need to input to the shell "python3 (folderName)/(codeName.codeType)" 
 delete the brackets and the words inside and replace with what you desire to input to the shell
