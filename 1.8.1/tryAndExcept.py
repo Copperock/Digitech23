6f48ff6ffd5aaf3d9565025b2250d8869f2a21bb
Replit user <>
2023-03-27 23:18:00 +0000

    Try and except

--- 1.8.1/tryAndExcept.py
+++ 1.8.1/tryAndExcept.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, "I'm words (english words)", 5]
+try:
+  numbers = [1, 2, "I'm words (english words)", 5]
 # Where you put said numbers 
-print(numbers)
+  print(numbers)
 # A thing that then prints the numbers
+  print("Hello worldd")
+  print()
+except:
+  print("Oh noes somthing brokey")
+finally:
+  print("Hello worlp")
