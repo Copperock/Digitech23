da06ea810f9a6931d5adb4bcb48e536fc878fab1
Replit user <>
2023-03-27 23:00:26 +0000

    Try n Except

---
+++ 1.8.1/tryAndExcept.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, "I'm words (english words)", 5]
+# Where you put said numbers 
+print(numbers)
+# A thing that then prints the numbers
---
+++ 1.8.1/tryNexcept.py
@@ -0,0 +1,9 @@
+AreWeThereyetstat = False
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  #print("Nothing went wrong")
+
+  
