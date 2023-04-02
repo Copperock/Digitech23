0a2602ac01fee39e7ec7a88ccbde0df660c67064
Replit user <>
2023-04-02 21:35:28 +0000

    Code outline

---
+++ Final Project/iteration1.py
@@ -0,0 +1,21 @@
+#Title: iteration1/number guessing game
+#Creater: Caleb Young
+#Current version: 0.1
+#StartDate: 4/3/23
+#FinishDate: 
+#Description: 
+
+
+#import time, numbers, and colour
+
+
+#ask name and greeting
+
+
+#level selection/start
+
+
+#number guessing
+
+
+#replay/quit
