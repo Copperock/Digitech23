f139b8eddf590024a93563f209c4ba86106b2c22
Replit user <>
2023-04-02 21:57:48 +0000

    Commenting code n such

--- Final Project/iteration1.py
+++ Final Project/iteration1.py
@@ -3,8 +3,7 @@
 #Current version: 0.1
 #StartDate: 4/3/23
 #FinishDate: 
-#Description: 
-
+#Description: Hopefully a file of which will contain the first version of a number guessing game that I will create.
 
 #import time, numbers, and colour
 
