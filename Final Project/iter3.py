8ecbe1aec13fc906df060afebba9eeebd9bc6870
Replit user <>
2023-05-11 02:59:00 +0000

    iter 3 broke

--- Final Project/iter3.py
+++ Final Project/iter3.py
@@ -5,19 +5,22 @@
 #FinishDate: 
 #Description: This is a file that is apart of a set of files that will in the end hopefullly contain a lovley polished game that works well.
 
-#importing stuff
+#random stuff that I just need to do
+replayLoop = False
 import random
 
 #Variables
-randintstart = 0
 randintend = 21
 attempt = 4
+
+#welcome
+def welcome():
+  print("Welcome to the random number guessing game. This is a game where you guess a number betwen the value of 0 and 20.")
 
 #game engine/attempts
 def gameEngineAndLives():
   global attempt
-  a = random.randint(randintstart,randintend)
-  print("Welcome to the random number guessing game. This is a game where you guess a number betwen the value of 0 and 20.")
+  a = random.randint(0,randintend)
   for i in range(attempt + 1):
       user_input = int(input("Enter Number: "))
   
@@ -28,6 +31,33 @@ def gameEngineAndLives():
           print(f'Try again! {attempt} attempts left.')
           attempt -= 1
           continue
+
+
+#replay loop?
+  def replaySYS():
+    global replayLoop
+    print("Now that we have finished that, would you like to play again?")
+    replay_input = input("Y,N    ").lower()
+    
   
-  
-gameEngineAndLives()
+    if replay_input == "y":
+      print("Let's play again")
+      replayLoop = False
+      gameEngineAndLives()
+    
+    else:
+      print("Goodbye")
+      replayLoop = True
+    
+  while replayLoop == False:
+    gameEngineAndLives()
+    replaySYS()
+
+#PLs work well
+def plsrun():
+  welcome()
+  gameEngineAndLives()
+  replaySYS()
+
+
+plsrun()
