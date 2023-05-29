43e3cae6c5ddb80ccd90cfc412f163f2f11580ed
Replit user <>
2023-05-29 23:52:56 +0000

    forgot a bit

--- Final Project/iter4.py
+++ Final Project/iter4.py
@@ -8,3 +8,74 @@
 # Description p2: set of files that will in the end
 # Descriptin p3: hopefullly contain a lovley polished game that works well.
 
+# random stuff that I just need to do
+import random
+# Variables
+randintend = 21
+attempt = 4
+
+
+# welcome
+
+
+print("Welcome to the random number guessing game.")
+print("This is a game where you guess a number between the value of 0 and 20.")
+
+
+# game engine and attempts
+
+
+def gameengineandlives():
+    """Return lives left and run game engine."""
+    global attempt
+    a = random.randint(0, randintend)
+    for i in range(attempt + 1):
+        user_input = int(input("Enter Number: "))
+
+        if user_input == (a):
+            print('You won')
+            break
+        else:
+            print(f'Try again! {attempt} attempts left.')
+            attempt -= 1
+            continue
+
+
+# replay loop
+replayloop = True
+
+
+def replaysys():
+    """Replay loop return yes or no info."""
+    global replayloop
+    print("Would you like to play again?")
+    replay_input = input("Y,N    ").lower()
+
+    if replay_input == "y":
+        print("Let's play again")
+        replayloop = True
+        gameengineandlives()
+    elif replay_input == "n":
+        print("Goodbye")
+        replayloop = False
+    else:
+        print("Please input Y or N")
+        replayloop = False
+        replaysys()
+
+# Please run well
+
+
+def plsrun():
+    """Do a run command to run the game."""
+    global attempt
+    attempt = 4
+    gameengineandlives()
+    replaysys()
+
+
+plsrun()
+
+# replayloop
+while replayloop:
+    plsrun()
