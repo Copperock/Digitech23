a771e1c4933ca6233a3434eea4912a0662446636
Replit user <>
2023-11-29 21:59:11 +0000

    desciptor added at top

--- Final Project/iter4.py
+++ Final Project/iter4.py
@@ -1,12 +1,15 @@
-"""This file is a number guessing game that is slowly being made."""
-# Title: iter4
-# Creater: Caleb Young
-# Current iteration: 4
-# StartDate: 30/5/23
-# FinishDate:
-# Description: This is a file that is apart of a
-# Description p2: set of files that will in the end
-# Descriptin p3: hopefullly contain a lovley polished game that works well.
+""".
+
+Title: iter4
+Creater: Caleb Young
+Current iteration: 4
+StartDate: 30/5/23
+FinishDate:
+
+Description: This is a guessing game that
+will create a random number, which has to
+be guessed with hints to compleate the game.
+"""
 
 # stuff to be imported
 import random
